151e7d6a52fa1c8212ac62c67fd2f9bede3e6517
livemehere <rhdxoals@gmail.com>
2026-07-20 04:09:59 +0900

    refactor: MeshRenderer

--- src/rendering/mesh/MeshRenderer.cpp
+++ src/rendering/mesh/MeshRenderer.cpp
@@ -4,16 +4,16 @@
 #include "materials/BasicMaterial.h"
 
 void MeshRenderer::render(const MeshRenderObject &object) const {
+    /* material specific */
+    object.material->bind();
 
+    /* all shader global */
     object.material->shader.bindUniformBlock("CameraData", UniformBinding::Camera);
     object.material->shader.bindUniformBlock("LightsData", UniformBinding::Lights);
 
+    /* mesh common */
     const glm::mat4 model = object.transform.getModelMatrix();
-    object.material->bind();
-
-    // ambient light
-    // object.material->shader.setVec3("uLightPos",context.lightPos);
-    // object.material->shader.setVec3("uViewPos",context.viewPos);
     object.material->shader.setMat4("uModel", model);
+
     object.mesh->draw();
 }
