9dd33aec99159fd6f9e7b9b2b731be46aacc8b2e
livemehere <rhdxoals@gmail.com>
2026-08-02 20:20:06 +0900

    chore: set clear color

--- src/rendering/Renderer.cpp
+++ src/rendering/Renderer.cpp
@@ -153,6 +153,7 @@ void Renderer::beginFrame(Scene &scene, const RenderExtent& size) {
     updateCameraBuffer(scene, size);
     updateLightsBuffer(scene);
 
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_DEPTH_TEST);
